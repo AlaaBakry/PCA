e52a71783a13a215683e3d1f12db56897793d636
AlaaBakry <s-alaa.mobarak@zewailcity.edu.eg>
2018-02-11 03:34:20 +0200

    edited

--- PCA_5.R
+++ PCA_5.R
@@ -1,11 +1,14 @@
 # this is written on R version 3.4.3 
 #PCA is done without transformation in this script as we need to keep unwanted variation "not sure" to analyze seperately as it stabilizes variance.
 # reading the data
+install.packages("readxl")
+library(readxl)
+read_excel
 setwd("C:/Users/amr01007778867/Desktop")
 TregData  <- read.delim("t_reg_only.txt")
 head(TregData)
 #in PCA We can Obtain the Eigenvectors and Eigenvalues from the covariance matrix (eigenvalue decomposition),or perform SVD like here
-princo <- prcomp(TregData, center=TRUE, scale=TRUE)
+princo <- prcomp(TregData, center=TRUE, scale=FALSE)
 summary(princo)
 # we have 32 components, the first component accounts for 82.54 % of the variation
 # to determine which components (have eigenvalues >1 ) to keep calculate eigenvalue 
@@ -13,8 +16,12 @@ princo$sdev ^ 2
 # only the first 2 have eigenvalues >1 and confirmed from graphs 
 screeplot(princo , main="Scree Plot", xlab="Components")
 screeplot(princo, main="Scree Plot", type="line" )
+boxplot(princo$x)
 
 # construct the projection matrix W from  the first principle component
-FPC <- princo$rotation[,1]
-
-
+#FPC <- princo$x[,1]
+trans<- t(princo)
+FPCT <- trans[,1]
+# test the correlation between FPCT and data
+#install.packages("ggpubr")
+data <- read.csv("metadata_treg.xlsx")
